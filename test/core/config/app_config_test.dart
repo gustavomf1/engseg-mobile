c6bf0a3ee3836beb2e73e44fe109de46e7399ad9
EngSeg Mobile Developer <gustavincraft03@gmail.com>
2026-05-19 13:18:51 -0300

    fix: tighten AppConfig test assertions

--- test/core/config/app_config_test.dart
+++ test/core/config/app_config_test.dart
@@ -3,7 +3,12 @@ import 'package:engseg_mobile/core/config/app_config.dart';
 
 void main() {
   test('AppConfig has correct base URLs', () {
-    expect(AppConfig.apiBaseUrl, contains('8080'));
-    expect(AppConfig.bffBaseUrl, contains('8081'));
+    expect(AppConfig.apiBaseUrl, equals('http://engseg-api:8080'));
+    expect(AppConfig.bffBaseUrl, equals('http://engseg-mobile-backend:8081'));
+  });
+
+  test('AppConfig has correct timeouts', () {
+    expect(AppConfig.connectTimeout, equals(const Duration(seconds: 30)));
+    expect(AppConfig.receiveTimeout, equals(const Duration(seconds: 60)));
   });
 }
